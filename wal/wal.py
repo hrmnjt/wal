2ed62bd2d5739046d14d94288478b9a504af3856
hrmnjt <harman@hrmnjt.dev>
2024-04-13 01:59:04 +0400

    feat: added sync_log functionality

--- wal/wal.py
+++ wal/wal.py
@@ -1,4 +1,5 @@
 from datetime import date
+from datetime import datetime
 from math import log10
 import argparse
 import configparser
@@ -9,6 +10,28 @@ import subprocess
 DEFAULT_CONFIG_PATH = "~/.config/wal/config.ini"
 DEFAULT_LOG_DIR = "~/.local/share/wal"
 DEFAULT_EDITOR = "nvim"
+# FIXME: remove personal info
+DEFAULT_GIT_REMOTE_URL = "git@github.com:hrmnjt/wal-test.git"
+# FIXME: remove personal info
+DEFAULT_GIT_USER_NAME = "hrmnjt"
+# FIXME: remove personal info
+DEFAULT_GIT_USER_EMAIL = "harman@hrmnjt.dev"
+
+
+def run_command(command, working_dir):
+    try:
+        result = subprocess.run(
+            command,
+            cwd=working_dir,
+            text=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e.stderr}")
+        return None
 
 
 def open_log(log_date: int, config):
@@ -55,21 +78,56 @@ def open_log(log_date: int, config):
     return None
 
 
-def sync_log():
-    print("syncing logs")
+def sync_log(message, config):
+    print(f"syncing logs using {config} and {message}")
+
+    log_dir = os.path.expanduser(config["DEFAULT"]["LOG_DIR"])
+    git_remote_url = config["DEFAULT"]["GIT_REMOTE_URL"]
+    git_user_name = config["DEFAULT"]["GIT_USER_NAME"]
+    git_user_email = config["DEFAULT"]["GIT_USER_EMAIL"]
+    git_dir = os.path.join(log_dir, ".git")
+    if not os.path.exists(git_dir):
+        print("Initializing Git repository...")
+        init_result = run_command(["git", "init"], log_dir)
+        if init_result:
+            print(init_result)
+
+        print(f"Setting remote repository to {config["DEFAULT"]["GIT_REMOTE_URL"]}")
+        set_remote_result = run_command(
+            ["git", "remote", "add", "origin", git_remote_url], log_dir
+        )
+        if set_remote_result:
+            print(set_remote_result)
+
+        print("Configuring user name and email for repository")
+        run_command(["git", "config", "user.name", git_user_name], log_dir)
+        run_command(["git", "config", "user.email", git_user_email], log_dir)
+
+    os.chdir(log_dir)
+
+    print("Adding all changes to staging area...")
+    run_command(["git", "add", "."], log_dir)
+
+    # TODO: get default message from config
+    default_commit_message = (
+        f"lge: updates from {datetime.now().strftime("%Y%m%d%H%M%S")}"
+    )
+    commit_message = message if message else default_commit_message
+    print("Committing changes...")
+    commit_result = run_command(["git", "commit", "-m", commit_message], log_dir)
+    if commit_result:
+        print(commit_result)
+
+    print("Pushing changes to remote repository...")
+    push_result = run_command(["git", "push", "-u", "origin", "main"], log_dir)
+    if push_result:
+        print(push_result)
 
     return None
 
 
 def find_log():
     print("finding logs")
-
-    return None
-
-
-def sync_subcommand(args):
-    print(args)
-    sync_log()
 
     return None
 
@@ -87,7 +145,13 @@ def parse_configuration(arg_config):
 
     config = configparser.ConfigParser()
     if not os.path.exists(config_path):
-        config["DEFAULT"] = {"LOG_DIR": DEFAULT_LOG_DIR, "EDITOR": DEFAULT_EDITOR}
+        config["DEFAULT"] = {
+            "LOG_DIR": DEFAULT_LOG_DIR,
+            "EDITOR": DEFAULT_EDITOR,
+            "GIT_REMOTE_URL": DEFAULT_GIT_REMOTE_URL,
+            "GIT_USER_NAME": DEFAULT_GIT_USER_NAME,
+            "GIT_USER_EMAIL": DEFAULT_GIT_USER_EMAIL,
+        }
         with open(config_path, "w") as config_file:
             config.write(config_file)
     else:
@@ -124,7 +188,6 @@ def parse_cli_arguments():
         metavar="message",
         help="sync logs with custom message",
     )
-    parser_sync.set_defaults(func=sync_subcommand)
 
     parser_find = subparsers.add_parser("find", help="find logs")
     parser_find.set_defaults(func=find_subcommand)
@@ -137,13 +200,11 @@ def main():
 
     config = parse_configuration(args.config)
 
-    if config:
-        print(config["DEFAULT"]["LOG_DIR"])
-        print(config["DEFAULT"]["EDITOR"])
-
     if args.subparser:
         if args.subparser == "open":
             open_log(args.date, config)
+        elif args.subparser == "sync":
+            sync_log(args.message, config)
         else:
             args.func(args)
     else:
